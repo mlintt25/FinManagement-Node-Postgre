bf2ed58f17045bd6822cb02a3f113d6fc2445531
acccuakien <acccuakien@gmail.com>
2025-05-08 09:37:01 +0700

    refactor: change logic response data after receive callback from google

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -15,7 +15,6 @@ import {
   RegisterResType
 } from '~/schemaValidations/auth.schema'
 import { TokenPayload } from '~/types/jwt.type'
-import envConfig from '~/configs'
 
 export const loginController = async (
   req: Request<ParamsDictionary, any, LoginBodyType>,
@@ -37,8 +36,15 @@ export const loginController = async (
 export const loginWithGoogleController = async (req: Request, res: Response, next: NextFunction) => {
   const { code } = req.query
   const result = await authService.loginWithGoogle(code as string)
-  const urlRedirect = `${envConfig.CLIENT_REDIRECT_CALLBACK}?access_token=${result.access_token}&refresh_token=${result.refresh_token}&new_user=${result.newUser}&verify=${result.verify}`
-  return res.redirect(urlRedirect)
+  // const urlRedirect = `${envConfig.CLIENT_REDIRECT_CALLBACK}?access_token=${result.access_token}&refresh_token=${result.refresh_token}&new_user=${result.newUser}&verify=${result.verify}`
+  return res.json({
+    message: result.newUser ? USERS_MESSAGES.REGISTER_SUCCESS : USERS_MESSAGES.LOGIN_SUCCESS,
+    data: {
+      accessToken: result.access_token,
+      refreshToken: result.refresh_token,
+      user: result.user
+    }
+  })
 }
 
 export const registerController = async (
